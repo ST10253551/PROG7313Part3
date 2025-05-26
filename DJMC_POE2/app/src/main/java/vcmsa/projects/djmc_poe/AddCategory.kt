ccdb60c6d47413aa9977dc6766a75094fdc6cb62
Calvin Bradshaw <131657826+ST10271221@users.noreply.github.com>
2025-05-26 13:20:28 +0200

    Calvin
    Firestore database code

---
+++ DJMC_POE2/app/src/main/java/vcmsa/projects/djmc_poe/AddCategory.kt
@@ -0,0 +1,32 @@
+package vcmsa.projects.djmc_poe
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+// AddCategory.kt
+class AddCategory : AppCompatActivity() {
+    private lateinit var titleInput: EditText
+    private val db = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_category)
+
+        titleInput = findViewById(R.id.categoryNameInput)
+        val saveButton: Button = findViewById(R.id.saveCategoryButton)
+
+        saveButton.setOnClickListener {
+            val title = titleInput.text.toString()
+            val category = Category(title)
+
+            db.collection("categories")
+                .add(category)
+                .addOnSuccessListener { finish() }
+                .addOnFailureListener { Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show() }
+        }
+    }
+}
